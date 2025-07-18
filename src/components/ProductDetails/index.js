09fc0f037626c2e9559f1fe57f9a83aff1bf899d
shohjahon-f41 <aizensolo086@gmail.com>
2025-07-18 21:35:18 +0500

    Sixth

--- src/components/ProductDetails/index.js
+++ src/components/ProductDetails/index.js
@@ -1,8 +1,18 @@
-export { default as ProDetIphone } from "./pro-det-iphone.png";
-export { default as ProDetIphone2 } from "./pro-det-iphone2.png";
-export { default as ProDetIphone3 } from "./pro-det-iphone3.png";
-export { default as ProDetIphone4 } from "./pro-det-iphone4.png";
-export { default as ProDetIphone5 } from "./pro-det-iphone5.png";
-export { default as ProDetIphoneBig2 } from "./pro-det-iphonebig2.png";
-export { default as ProDetIphoneBig3 } from "./pro-det-iphonebig3.png";
-export { default as ProDetIphoneBig4 } from "./pro-det-iphonebig4.png";
+export { default as ProDetIphone }
+from "./pro-det-iphone.png";
+export { default as ProDetIphone2 }
+from "./pro-det-iphone2.png";
+export { default as ProDetIphone3 }
+from "./pro-det-iphone3.png";
+export { default as ProDetIphone4 }
+from "./pro-det-iphone4.png";
+export { default as ProDetIphone5 }
+from "./pro-det-iphone5.png";
+export { default as ProDetIphoneBig }
+from "./pro-det-iphonebig.png";
+export { default as ProDetIphoneBig2 }
+from "./pro-det-iphonebig2.png";
+export { default as ProDetIphoneBig3 }
+from "./pro-det-iphonebig3.png";
+export { default as ProDetIphoneBig4 }
+from "./pro-det-iphonebig4.png";
