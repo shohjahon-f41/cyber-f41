61fff1819bd3cef58991bf0309872eb72bed0d72
shohjahon-f41 <aizensolo086@gmail.com>
2025-08-16 12:40:13 +0500

    19:56

--- src/contexts/CartContext.jsx
+++ src/contexts/CartContext.jsx
@@ -4,11 +4,15 @@ export const CartContext = createContext(null);
 
 function CartProvider({ children }) {
   const [cart, setCart] = useState([]);
+  /**
+   *
+   * @param {*} data
+   */
   function addCart(data) {
     if (cart.some((item) => item.id === data.id)) {
       setCart(
         cart.map((item) =>
-          item.id === data.id ? { ...item, qty: (item.qty += 1) } : {...item}
+          item.id === data.id ? { ...item, qty: (item.qty += 1) } : { ...item }
         )
       );
     } else {
@@ -16,9 +20,47 @@ function CartProvider({ children }) {
       setCart([...cart, obj]);
     }
   }
+
+  function incrementData(data) {
+    /** Increment */
+    setCart(
+      cart.map((item) =>
+        item.id === data.id ? { ...item, qty: (item.qty += 1) } : { ...item }
+      )
+    );
+  }
+
+  function decrementData(data) {
+    /** Decrement */
+    setCart(
+      cart.map((item) =>
+        item.id === data.id
+          ? { ...item, qty: item.qty === 1 ? 1 : (item.qty -= 1) }
+          : { ...item }
+      )
+    );
+  }
+
+  function deleteItem(data) {
+    setCart(cart.filter((item) => item.id !== data.id));
+  }
+
+  function deleteAll() {
+    setCart([])
+  }
+
   console.log(cart);
   return (
-    <CartContext.Provider value={{ addCart, cart }}>
+    <CartContext.Provider
+      value={{
+        addCart,
+        cart,
+        incrementData,
+        decrementData,
+        deleteItem,
+        deleteAll,
+      }}
+    >
       {children}
     </CartContext.Provider>
   );
