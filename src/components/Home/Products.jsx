284a217b7c11f8036f855ee71bc4ac752be141eb
shohjahon-f41 <aizensolo086@gmail.com>
2025-08-26 16:25:24 +0500

    ok

--- src/components/Home/Products.jsx
+++ src/components/Home/Products.jsx
@@ -1,11 +1,21 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {ProductsItem} from '../../data/data';
 import {CartContext} from '../../contexts/CartContext';
 import {LanguageContext} from '../../contexts/LanguageContext';
+import {Link} from 'react-router-dom';
 
 function Products() {
+	const [products, setProducts] = useState([]);
 	const {addCart} = useContext(CartContext);
 	const {language} = useContext(LanguageContext);
+
+	function getProducts() {
+		fetch('https://302d37aacffa4da5.mokky.dev/products')
+			.then((res) => res.json())
+			.then((json) => setProducts(json));
+	}
+
+	useEffect(() => {getProducts()}, [])
 
 	// console.log(CartContext);
 
@@ -23,9 +33,9 @@ function Products() {
 							{ProductsItem.map((item) => (
 								<div className="products-cards-item" key={item.id}>
 									<div className="products-item-icon">{item.svg}</div>
-									<div className="products-item-img">
+									<Link className="products-item-image" to={`/product-details/${item.id}`}>
 										<img src={item.img} alt="Iphone" />
-									</div>
+									</Link>
 									<div className="products-item-sub">
 										<p className="products-item-subtitle">{language === 'uz' ? item.name_uz : item.name}</p>
 										<p className="products-item-buy">{item.buy}</p>
--- src/data/routes.jsx
+++ src/data/routes.jsx
@@ -1,39 +1,41 @@
-import { Cart, CartStepOne, CartStepTwo, Favourite, Home, Products, ProductDetails } from "../pages";
-import CartStepThree from "../pages/CartStepThree";
+import {Cart, CartStepOne, CartStepTwo, Favourite, Home, Products, ProductDetails} from '../pages';
+import CartStepThree from '../pages/CartStepThree';
 
 export const routes = [
-    {
-        path: "/",
-        element: <Home />
-    },
-    {
-        path: "/products",
-        element: <Products />
-    },
-    { path: "/product-details/:id", element: <ProductDetails /> },
-    {
-        path: "/cart",
-        element: <Cart />
-    },
-    {
-        path: "/favourite",
-        element: <Favourite />
-    },
-    {
-        path:"/cart/first-step",
-        element: <CartStepOne />
-    },
-    {
-        path:"/cart/second-step",
-        element: <CartStepTwo />
-    },
-    {
-        path:"/cart/third-step",
-        element: <CartStepThree />
-    },
-    {
-        path: "*",
-        element: <Home />
-    }
-    
-]
+	{
+		path: '/',
+		element: <Home />,
+	},
+	{
+		path: '/products',
+		element: <Products />,
+	},
+	{
+		path: '/product-details/:id',
+		element: <ProductDetails />,
+	},
+	{
+		path: '/cart',
+		element: <Cart />,
+	},
+	{
+		path: '/favourite',
+		element: <Favourite />,
+	},
+	{
+		path: '/cart/first-step',
+		element: <CartStepOne />,
+	},
+	{
+		path: '/cart/second-step',
+		element: <CartStepTwo />,
+	},
+	{
+		path: '/cart/third-step',
+		element: <CartStepThree />,
+	},
+	{
+		path: '*',
+		element: <Home />,
+	},
+];
